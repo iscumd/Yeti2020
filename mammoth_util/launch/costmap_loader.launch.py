60e4ad681ac93ac3cb527282b878658ebc6f1383
dcutting133 <dcutting@umich.edu>
2021-05-28 18:34:06 -0400

    fix path

--- mammoth_util/launch/costmap_loader.launch.py
+++ mammoth_util/launch/costmap_loader.launch.py
@@ -17,9 +17,13 @@ def generate_launch_description():
     # ROS packages
     pkg_mammoth_util = get_package_share_directory('mammoth_util')
 
+    # LOADED COSTMAP HERE
+    costmap_img_filename = 'costmap.pgm' # just the file name no path
+    # LOADED COSTMAP HERE
+
     config_dir = os.path.join(pkg_mammoth_util, 'config')
-    costmaps_dir = os.path.join(pkg_mammoth_util, 'costmaps')
-    costmap_filepath = os.path.join(costmaps_dir, 'costmap.pgm')
+    costmaps_dir = os.path.join(config_dir, 'costmaps')
+    costmap_filepath = os.path.join(costmaps_dir, costmap_img_filename)
 
     # Nodes
     costmap_loader = Node(
